01ec22d463b1f1d31c2b52745105b82c32370137
Daniel Krajnak <dan.krajnak@gmail.com>
2021-02-14 00:58:10 -0500

    Add less support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from "next/app";
-import "../styles/globals.css";
-import "antd/dist/antd.css";
+import "../Styles/dist.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
