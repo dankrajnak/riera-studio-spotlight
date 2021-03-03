f72d80f56efe88e55660c73522472d69cb741faf
Daniel Krajnak <dan.krajnak@gmail.com>
2021-03-02 23:40:11 -0500

    Add prismic rage

---
+++ src/PrismicRage/Readme.md
@@ -0,0 +1,1 @@
+Prismic's graphql schema is really really lacking, so I needed to created this service layer to create basic typing for my components. Prismic has a lot of promising features, and a great free tier, but goddamnit this sucks.
---
+++ src/PrismicRage/shared.ts
@@ -0,0 +1,30 @@
+import { Exhibition } from "../generated/graphql";
+
+export type PrismicRageImage = {
+  alt: string | null;
+  copyright: string | null;
+  dimensions: { width: number; height: number };
+  url: string;
+};
+
+export const getPrismicRageImage = (img: any): PrismicRageImage => {
+  if (!img.url || typeof img.url !== "string") {
+    throw new Error("img doesn't have required img property");
+  }
+  let url: string = img.url;
+  const questionIndex = url.indexOf("?");
+  if (questionIndex !== -1) {
+    url = url.substr(0, questionIndex);
+  }
+  return {
+    ...img,
+    url,
+  };
+};
+
+export type PrismicRageExhibition = Omit<
+  Omit<Exhibition, "main_image">,
+  "_meta"
+> & {
+  main_image?: PrismicRageImage;
+};
