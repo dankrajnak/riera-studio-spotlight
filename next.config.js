d568b38fd05d5c43cad3423f143a02505a185830
Daniel Krajnak <dan.krajnak@gmail.com>
2021-04-10 22:17:38 -0400

    Commit WIP before new homepage idea

--- next.config.js
+++ next.config.js
@@ -3,13 +3,4 @@ module.exports = {
     loader: "imgix",
     path: "",
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(graphql|gql)$/,
-      exclude: /node_modules/,
-      loader: "graphql-tag/loader",
-    });
-
-    return config;
-  },
 };
