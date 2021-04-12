e95e1ff8e238dd10ebca5cbdfbda7e18cab9fd88
Daniel Krajnak <dan.krajnak@gmail.com>
2021-04-12 02:04:16 -0400

    Quick fixes to TS errors

--- src/Utils/SplitText.tsx
+++ src/Utils/SplitText.tsx
@@ -1,24 +1,29 @@
 import { motion } from "framer-motion";
 
-const SplitText = ({ children, ...rest }: { children: string }) => {
+// TODO this is just hacked together
+const SplitText = ({ children, ...rest }: any) => {
   const words = children.split(" ");
-  return words.map((word, i) => (
-    <div
-      key={children + i}
-      style={{ display: "inline-block", overflow: "hidden" }}
-    >
-      <motion.h1
-        {...rest}
-        style={{
-          display: "inline-block",
-          willChange: "transform",
-        }}
-        custom={i}
-      >
-        {word + (i !== words.length - 1 ? "\u00A0" : "")}
-      </motion.h1>
-    </div>
-  ));
+  return (
+    <>
+      {words.map((word, i) => (
+        <div
+          key={children + i}
+          style={{ display: "inline-block", overflow: "hidden" }}
+        >
+          <motion.h1
+            {...rest}
+            style={{
+              display: "inline-block",
+              willChange: "transform",
+            }}
+            custom={i}
+          >
+            {word + (i !== words.length - 1 ? "\u00A0" : "")}
+          </motion.h1>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default SplitText;
